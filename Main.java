5dcceb4d7979267525684a6f82f7f40fd927d2af
AhmadShekibHaidari <ahmadshekibhaidari82@gmail.com>
2024-11-27 18:00:21 +0330

    MAIN CALSS IMPELMENTED

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Eagle e1 = new Eagle("Elone Mask ",4,600,"STRONG AND STRONG: ");
+        Parrot p1 = new Parrot("Mark",8,10,"Nice voice");
+        e1.hunt(p1);
+    }
+}
+

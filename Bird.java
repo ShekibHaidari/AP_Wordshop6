3f0181211c820a1744db44fd50631d570c2d3a69
AhmadShekibHaidari <ahmadshekibhaidari82@gmail.com>
2024-11-27 17:48:14 +0330

    BIRD AND MAMMEL CLASS MAKED

--- Bird.java
+++ Bird.java
@@ -1,2 +1,29 @@
-public class Bird {
+public abstract class Bird extends Animal  {
+    private int height;
+    private String description;
+
+    public Bird(String name,int age,int height, String description) {
+        super(name, age);
+        this.height = height;
+        this.description = description;
+
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + age + " " + height + " " + description;
+    }
+
 }
--- Mammel.java
+++ Mammel.java
@@ -1,3 +1,27 @@
-public class Mammel {
+public abstract class Mammel extends Animal {
+    private int speed;
+    private String description;
+    public Mammel( String name, int age, int speed, String description) {
+        super(name,age);
+        this.speed = speed;
+        this.description = description;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + age + " " + speed + " " + description;
+    }
 }
